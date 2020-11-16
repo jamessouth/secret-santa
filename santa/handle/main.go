7f21681923ee9f53d55ab69830471cf3c5d122ec
jamessouth <jamesdanielsouth@gmail.com>
2020-11-16 01:08:28 -0600

    cont lambda

--- santa/handle/main.go
+++ santa/handle/main.go
@@ -1,48 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
-
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
+	fmt.Println("bod", req.Body)
+	fmt.Println("meth", req.HTTPMethod)
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Helloooo, %v", string(ip)),
+		Body:       fmt.Sprintf("Helloooo, %v", "hi"),
 		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
 	}, nil
 }
 
